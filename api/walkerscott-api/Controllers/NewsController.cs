db5afbc14a2a929a5f43309448d3998f248b55b4
PRATEEK KARNA <prateekkarna13@live.com>
2023-08-11 17:54:58 +0530

    api chages

---
+++ api/walkerscott-api/Controllers/NewsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using walkerscott_api.Response;
+using walkerscott_application.Dto;
+using walkerscott_application.Query.Interfaces;
+
+namespace walkerscott_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly INewsQuery _newsQuery;
+        public NewsController(INewsQuery newsQuery)
+        {
+            _newsQuery = newsQuery;
+        }
+
+        [HttpGet("GetNewsByPage")]
+        public async Task<IActionResult> GetNewsByPage(int pageNo, int perPage)
+        {
+            try
+            {
+                var newsArticles = await _newsQuery.GetByPage(pageNo, perPage);
+                ApiResponse<GetNewsResponseDto> apiResponse = new ApiResponse<GetNewsResponseDto>()
+                { 
+                    Data = newsArticles,
+                    IsSuccess = true,
+                    StatusCode = 200
+                };
+
+                return new OkObjectResult(apiResponse);
+
+            }
+
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ api/walkerscott-api/Response/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace walkerscott_api.Response
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+
+        public T? Data { get; set; }
+
+        public Errors? Errors { get; set; }
+
+    }
+
+    public class Errors
+    {
+        public List<string>? ErrorDescription { get; set; }
+    }
+}
